e8f98f366d0cfa42a170b5d42256028a37047808
jerryputrapratama <jerrypratama177@gmail.com>
2023-12-20 15:51:50 +0700

    inputData

--- table.js
+++ table.js
@@ -1,2 +1,25 @@
-const halo = "halo"
-console.log(halo)
+function tambahData() {
+  var flight = document.getElementById("id_flight").value;
+  var flt = document.getElementById("id_flt").value;
+  var reg = document.getElementById("id_reg").value;
+  var date = document.getElementById("id_date").value;
+  var dep = document.getElementById("id_dep").value;
+
+  document.getElementById("tabel-main");
+  var row = table.insertRow(-1);
+
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+
+  cell1.innerHTML = flight;
+  cell2.innerHTML = flt;
+  cell3.innerHTML = reg;
+  cell4.innerHTML = date;
+  cell5.innerHTML = dep;
+
+  // Membersihkan formulir setelah menambahkan data
+  document.getElementById("myForm").reset();
+}
